8775e34227a81b90f553dab1d0e4270c63ec206b
davidkachor <kachor.david@gmail.com>
2022-10-06 16:44:44 +0300

    add types

---
+++ ui/src/MOCHA_DATA.ts
@@ -0,0 +1,82 @@
+import Product from './types/product'
+
+export const MOCHA_DATA: Product[] = [
+	{
+		id: '1',
+		imageUrl:
+			'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imgacademy.com%2Fabout-img-academy&psig=AOvVaw3otf9POwuLVpzno8Io6I13&ust=1665150146366000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOC4hurdy_oCFQAAAAAdAAAAABAD',
+		count: 2,
+		comments: [],
+		name: 'Hello World',
+		size: {
+			height: 20,
+			width: 40,
+		},
+		weight: '120kg',
+	},
+    {
+        id: '2',
+        imageUrl:
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imgacademy.com%2Fabout-img-academy&psig=AOvVaw3otf9POwuLVpzno8Io6I13&ust=1665150146366000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOC4hurdy_oCFQAAAAAdAAAAABAD',
+        count: 2,
+        comments: [],
+        name: 'Hello World',
+        size: {
+            height: 20,
+            width: 40,
+        },
+        weight: '120kg',
+    },
+    {
+        id: '3',
+        imageUrl:
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imgacademy.com%2Fabout-img-academy&psig=AOvVaw3otf9POwuLVpzno8Io6I13&ust=1665150146366000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOC4hurdy_oCFQAAAAAdAAAAABAD',
+        count: 2,
+        comments: [],
+        name: 'Hello World',
+        size: {
+            height: 20,
+            width: 40,
+        },
+        weight: '120kg',
+    },
+    {
+        id: '4',
+        imageUrl:
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imgacademy.com%2Fabout-img-academy&psig=AOvVaw3otf9POwuLVpzno8Io6I13&ust=1665150146366000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOC4hurdy_oCFQAAAAAdAAAAABAD',
+        count: 2,
+        comments: [],
+        name: 'Hello World',
+        size: {
+            height: 20,
+            width: 40,
+        },
+        weight: '120kg',
+    },
+    {
+        id: '5',
+        imageUrl:
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imgacademy.com%2Fabout-img-academy&psig=AOvVaw3otf9POwuLVpzno8Io6I13&ust=1665150146366000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOC4hurdy_oCFQAAAAAdAAAAABAD',
+        count: 2,
+        comments: [],
+        name: 'Hello World',
+        size: {
+            height: 20,
+            width: 40,
+        },
+        weight: '120kg',
+    },
+    {
+        id: '6',
+        imageUrl:
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imgacademy.com%2Fabout-img-academy&psig=AOvVaw3otf9POwuLVpzno8Io6I13&ust=1665150146366000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOC4hurdy_oCFQAAAAAdAAAAABAD',
+        count: 2,
+        comments: [],
+        name: 'Hello World',
+        size: {
+            height: 20,
+            width: 40,
+        },
+        weight: '120kg',
+    },
+]
