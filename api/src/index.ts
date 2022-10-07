c92774872db30517eba839e6508149dd9bcb3130
davidkachor <kachor.david@gmail.com>
2022-10-07 08:35:56 +0300

    init api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,8 @@
-import './pre-start'; // Must be the first import
-import logger from 'jet-logger';
-
-import envVars from '@shared/env-vars';
-import server from './server';
-
+import server from "./server";
 
 // **** Start server **** //
 
-const msg = ('Express server started on port: ' + envVars.port.toString());
-server.listen(envVars.port, () => logger.info(msg));
+const PORT = 5000;
+
+const msg = `Express server started on port: ${PORT}`;
+server.listen(PORT, () => console.log(msg));
